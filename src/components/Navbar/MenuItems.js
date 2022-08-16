0fd8a83290e714c19e706bc68979bc3e148e3f0a
ndeguchi <natsudeguchi@gmail.com>
2022-08-16 10:36:17 -0700

    Initial commit

---
+++ src/components/Navbar/MenuItems.js
@@ -0,0 +1,14 @@
+export const MenuItems = [
+     {
+          title: 'Home',
+          url: '#',
+          cName: 'nav-links'
+     },
+     {
+          title: 'Search',
+          url: '#',
+          cName: 'nav-links'
+     },
+
+     
+]
